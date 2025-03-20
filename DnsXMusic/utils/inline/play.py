47447e346a46444b75480db205d3a950e64a5119
BabyXmusics <cutemusic008@gmail.com>
2025-03-20 16:49:06 +0530

    Baby #440

--- DnsXMusic/utils/inline/play.py
+++ DnsXMusic/utils/inline/play.py
@@ -69,14 +69,14 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
           [
             InlineKeyboardButton(
-                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/Music_World_Update"
+                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/BabyMusicUpdate"
             ),
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="𝖢𝗅𝗈𝗌𝖾"
           ),
          
         
             InlineKeyboardButton(
-                text="𝐒υρρσɾƚ", url=f"https://t.me/Music_World_Support"),
+                text="𝐒υρρσɾƚ", url=f"https://t.me/the_cute_boy_op"),
         ],
     ]
     return buttons
@@ -86,14 +86,14 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
           [
             InlineKeyboardButton(
-                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/Music_World_Update"
+                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/BabyMusicUpdate"
             ),
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="𝖢𝗅𝗈𝗌𝖾"
           ),
          
         
             InlineKeyboardButton(
-                text="𝐒υρρσɾƚ", url=f"https://t.me/Music_World_Support"),
+                text="𝐒υρρσɾƚ", url=f"https://t.me/the_cute_boy_op"),
         ],
     ]
     return buttons
@@ -127,14 +127,14 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
           [
             InlineKeyboardButton(
-                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/Music_World_Update"
+                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/BabyMusicUpdate"
             ),
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="𝖢𝗅𝗈𝗌𝖾"
           ),
          
         
             InlineKeyboardButton(
-                text="𝐒υρρσɾƚ", url=f"https://t.me/Music_World_Support"),
+                text="𝐒υρρσɾƚ", url=f"https://t.me/the_cute_boy_op"),
         ],
     ]
     return buttons
@@ -144,14 +144,14 @@ def telegram_markup(_, chat_id):
     buttons = [
           [
             InlineKeyboardButton(
-                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/Music_World_Update"
+                text="𝐔ρ𝖽αтєѕ", url=f"https://t.me/BabyMusicupdate"
             ),
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="𝖢𝗅𝗈𝗌𝖾"
           ),
          
         
             InlineKeyboardButton(
-                text="𝐒υρρσɾƚ", url=f"https://t.me/Music_World_Support"),
+                text="𝐒υρρσɾƚ", url=f"https://t.me/the_cute_boy_op"),
         ],
     ]
     return buttons
